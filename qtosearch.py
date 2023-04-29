e37156f3fc65cfe2c5540cf6eb2f31e48fdeb51c
swickape <patrick.swickard@amplyfigroup.com>
2023-04-29 14:51:22 -0400

    french language source harvest 10rpp

--- qtosearch.py
+++ qtosearch.py
@@ -7,15 +7,42 @@ import csv
 qidfile = open("qidhash.json",'r')
 qidhash = json.loads(qidfile.read())
 
+thisqidlist = []
+for qid in list(qidhash.keys()):
+    language = qidhash.get(qid).get('language')
+    print(language)
+    if language == 'French (Q150)':
+      thisqidlist.append(qid)
 
-thisqid = 'Q5352174'
+#thisqidlist = ['Q16979942']
 
-this_base_url = qidhash[thisqid]['base_url']
-this_query = 'agua'
-this_request_url = this_base_url + '/wp-json/wp/v2/posts?search=' + this_query
+article_list = []
 
-print(this_base_url)
-print(this_query)
-result = requests.get(this_request_url,timeout=20)
+for thisqid in thisqidlist:
+  this_base_url = qidhash[thisqid]['base_url']
+  this_query = 'eau'
+  rpp = '10' # 100 works but kinda big
+  order_by = 'date'
+  order = 'desc'
+  this_request_url = this_base_url + '/wp-json/wp/v2/posts?search=' + this_query + '&per_page=' + rpp + '&orderby=' + order_by + '&order=' + order
+  #print(this_request_url)
+  #print(this_base_url)
+  print(this_query)
+  try:
+    result = requests.get(this_request_url,timeout=20)
+    article_hash = json.loads(result.text)
+  except:
+    continue
 
-print(result.text)
+  for this_article in article_hash:
+    title = this_article.get('title',{}).get('rendered')
+    article_link = this_article.get('link',{})
+    content = this_article.get('content',{}).get('rendered')
+    print(title)
+    #print(article_link)
+#    print(content)
+    article_list.append(this_article)
+
+outfile = open("french_source_eau_search.json",'w')
+outfile.write(json.dumps(article_list))
+outfile.close
