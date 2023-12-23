2f2cb78fc646c32160155e11b2fd917796c53d1f
Patrick Swickard <swickape@hotmail.com>
2023-12-23 12:01:52 -0500

    structure explorer pylint fixes

--- structure_explorer.py
+++ structure_explorer.py
@@ -1,43 +1,38 @@
-import os
+"""Explore structure and keywords"""
 import json
 import re
-import requests
 
-outfile = open("outfile.txt",'r')
+with open('outfile.txt','r',encoding='utf-8') as myinfile:
+  outlist = json.loads(myinfile.read())
 
-outlist = json.loads(outfile.read())
-
-outfile.close()
-
-taghash = {} 
+taghash = {}
 
 count = 0
 for thisfilename in outlist:
-    count += 1
-    print(count)
-    thisfile = open('wikidata/' + thisfilename)
+  count += 1
+  print(count)
+  with open('wikidata/' + thisfilename,'r',encoding='utf-8') as thisfile:
     thishash = json.loads(thisfile.read())
-    thisfile.close()
-    title = thishash['title']
-    print(title)
-    description = thishash['description']
-    print(description)
-    base_url = thishash['claims']['P856'][0]
-    print(base_url)
-    full_url = base_url + '/wp-json/wp/v2/posts?search='
-    print(full_url)
-    try:
-      exhtml = thishash['exhtml']
-      print(exhtml)
-    except:
-      pass
-    print('************')
-    for thislabel in thishash['labels']:
-        if re.search(r"^Q",thislabel):
-          label = thishash['labels'][thislabel]
-          taghash[label] = 1
-    print('************')
+  title = thishash['title']
+  print(title)
+  description = thishash['description']
+  print(description)
+  base_url = thishash['claims']['P856'][0]
+  print(base_url)
+  full_url = base_url + '/wp-json/wp/v2/posts?search='
+  print(full_url)
+  try:
+    exhtml = thishash['exhtml']
+    print(exhtml)
+  except:
+    pass
+  print('************')
+  for thislabel in thishash['labels']:
+    if re.search(r"^Q",thislabel):
+      label = thishash['labels'][thislabel]
+      taghash[label] = 1
+  print('************')
 taglist = list(taghash.keys())
 print(taglist)
 for thistag in taglist:
-    print(thistag)
+  print(thistag)
