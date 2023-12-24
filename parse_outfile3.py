1f7eddd7f5405300a327271353208f36e64ac785
Patrick Swickard <swickape@hotmail.com>
2023-12-24 11:06:20 -0500

    parse outfile3 pylint improvements

--- parse_outfile3.py
+++ parse_outfile3.py
@@ -1,52 +1,43 @@
-import os
+"""Explore confirmed spam etc"""
 import json
-import re
-import requests
 import csv
 
-confirmed_file = open("outfile_wpconfirmed.txt",'r')
-
-confirmed_list = json.loads(confirmed_file.read())
-
-confirmed_file.close()
+with open('outfile_wpconfirmed.txt','r',encoding='utf-8') as myinfile:
+  confirmed_list = json.loads(myinfile.read())
 
 spam_set = set()
 
-with open("spam.txt",'r') as spam_file:
-  reader = csv.reader(spam_file)
-  for row in reader:
-     spam_set.add(row[0])
-spam_file.close()
-
-#unresponsive_list = []
-#wordpress_site_list = []
-#wordpress_nonsite_list = []
+with open("spam.txt",'r',encoding='utf-8') as myinfile:
+  reader = csv.reader(myinfile)
+for row in reader:
+  spam_set.add(row[0])
 
 wordpress_legitimate_list = []
 
 count = 0
 for thisfilename in confirmed_list:
-    count += 1
-    print(thisfilename)
-    print(count)
-    if thisfilename in spam_set:
-        print("THIS IS SPAM SITE")
-    else:
-      thisfile = open('wikidata/' + thisfilename)
-      thishash = json.loads(thisfile.read())
-      thisfile.close()
-      title = thishash['title']
-      print(title)
-      description = thishash['description']
-      print(description)
-      base_url = thishash['claims']['P856'][0]
-      print(base_url)
-      full_url = base_url + '/wp-json/wp/v2/posts?search='
-      print(full_url)
-      wordpress_legitimate_list.append(thisfilename)
-      #inp = input('press any key to continue')
-      print('************')
-outfile = open("outfile_wplegit.txt",'w')
-outfile.write(json.dumps(wordpress_legitimate_list))
-outfile.close
+  count += 1
+  print(thisfilename)
+  print(count)
+  if thisfilename in spam_set:
+    print("THIS IS SPAM SITE")
+  else:
+    with open('wikidata/' + thisfilename,'r',encoding='utf-8') as myinfile:
+      thishash = json.loads(myinfile.read())
+    title = thishash['title']
+    print(title)
+    description = thishash['description']
+    print(description)
+    base_url = thishash['claims']['P856'][0]
+    print(base_url)
+    full_url = base_url + '/wp-json/wp/v2/posts?search='
+    print(full_url)
+    wordpress_legitimate_list.append(thisfilename)
+    #inp = input('press any key to continue')
+    print('************')
+#outfile = open("outfile_wplegit.txt",'w')
+#outfile.write(json.dumps(wordpress_legitimate_list))
+#outfile.close
+with open("outfile_wplegit.txt",'w',encoding='utf-8') as myoutfile:
+  myoutfile.write(json.dumps(wordpress_legitimate_list))
 print(wordpress_legitimate_list)
