0d8b4a34764914490583a603d2a5d12ad175ecbd
Patrick Swickard <swickape@hotmail.com>
2023-12-29 19:10:02 -0500

    pylint improvements

--- fetch_international_newspapers.py
+++ fetch_international_newspapers.py
@@ -1,16 +1,17 @@
-import mkwikidata
+"""Fetch newspapers from wikidata"""
 import json
+import mkwikidata
 
 # This is a query to select wikidata items that are an "instance of" (P31) "newspaper" (Q11032)
 # written so that the items are obliged to have an "official website" (P154)
-# along with optional fields "country" (P17) "place of publication" (P291) "language of work or name" (P407)
-# and "logo image" (P154)
+# along with optional fields "country" (P17) "place of publication" (P291)
+# "language of work or name" (P407) and "logo image" (P154)
 
 # The structure of this query should make it clear how this can be modified to include
 # other required or optional fields
 # For further SPARQL syntax see https://www.wikidata.org/wiki/Wikidata:SPARQL_tutorial
 
-query = """
+QUERY = """
 SELECT ?item ?itemLabel ?country ?countryLabel ?place ?placeLabel ?language ?languageLabel ?pic ?offurl
 WHERE
 {
@@ -23,7 +24,7 @@ OPTIONAL {?item wdt:P154 ?pic .}
 SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en" }
 }
 """
-query_result = mkwikidata.run_query(query, params={ })
+query_result = mkwikidata.run_query(QUERY, params={ })
 
 print(json.dumps(query_result))
 
@@ -31,4 +32,3 @@ print(json.dumps(query_result))
 #outfile = open("full_list_of_newspapers.json",'w')
 #outfile.write(json.dumps(query_result))
 #outfile.close
-
--- grab_nolang4.py
+++ grab_nolang4.py
@@ -1,8 +1,6 @@
-import os
+"""Script to find wikidata without language"""
 import json
 import re
-import requests
-import csv
 import mkwikidata
 
 query = """
@@ -20,9 +18,8 @@ SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en" 
 """
 query_result = mkwikidata.run_query(query, params={ })
 
-outfile = open("wikidata_query_result.json",'w')
-outfile.write(json.dumps(query_result))
-outfile.close
+with open('wikidata_query_result.json','w',encoding='utf-8') as myoutfile:
+  myoutfile.write(json.dumps(query_result))
 
 language_hash = {}
 
@@ -42,80 +39,65 @@ for thisitem in biglist:
   else:
     language_hash[id2] = language1
 
-legit_file = open("outfile_wplegit.txt",'r')
-
-legit_list = json.loads(legit_file.read())
-
-legit_file.close()
+with open('outfile_wplegit.txt','r',encoding='utf-8') as myinfile:
+  legit_list = json.loads(myinfile.read())
 
 wordpress_legitimate_list = []
 
 count = 0
 langhash = {}
-print
 
-
-def fetch_language(id):
-  return language_hash[id]
+def fetch_language(thisid):
+  """Fetch language from id"""
+  return language_hash[thisid]
 
 for thisfilename in legit_list:
-    thisfile = open('wikidata/' + thisfilename)
-    thishash = json.loads(thisfile.read())
-    thisfile.close()
-    #print(thisfilename)
-    id = thishash['wikibase']
-    language = thishash['wikidata'].get('language of work or name (P407)','None')
-    #print(language)
-    #language = thishash['wikidata']['language of work or name (P407)']
-    if type(language) is list:
-        language = language[0]
-    if not language == 'None':
-      print(language)
-      if language in list(langhash.keys()):
-        langhash[language] += 1
-      else:
-        langhash[language] = 1
-    else:
-      language = fetch_language(id)
-      print(language)
-      if language in list(langhash.keys()):
-        langhash[language] += 1
-      else:
-        langhash[language] = 1
-    count += 1
-    print(count)
-    print('https://www.wikidata.org/wiki/' + id)
-    title = thishash['title']
-    print(title)
-    description = thishash['description']
-    print(description)
-    base_url = thishash['claims']['P856']
-    if type(base_url) is list:
-        base_url = base_url[0]
-    base_url2 = thishash['wikidata']['official website (P856)']
-    if type(base_url2) is list:
-        base_url2 = base_url2[0]
-    if base_url != base_url2:
-      print('Something went wrong')
-      print(base_url)
-      print(base_url2)
-      break
+  with open('wikidata/' + thisfilename,'r',encoding='utf-8') as myinfile:
+    thishash = json.loads(myinfile.read())
+  #print(thisfilename)
+  myid = thishash['wikibase']
+  language = thishash['wikidata'].get('language of work or name (P407)','None')
+  #print(language)
+  #language = thishash['wikidata']['language of work or name (P407)']
+  if isinstance(language,list):
+    language = language[0]
+  if language == 'None':
+    language = fetch_language(myid)
+  print(language)
+  langhash[language] = langhash.get(language,0) + 1
+  count += 1
+  print(count)
+  print('https://www.wikidata.org/wiki/' + myid)
+  title = thishash['title']
+  print(title)
+  description = thishash['description']
+  print(description)
+  base_url = thishash['claims']['P856']
+  if isinstance(base_url,list):
+    base_url = base_url[0]
+  base_url2 = thishash['wikidata']['official website (P856)']
+  if isinstance(base_url2,list):
+    base_url2 = base_url2[0]
+  if base_url != base_url2:
+    print('Something went wrong')
     print(base_url)
     print(base_url2)
-    full_url = base_url + '/wp-json/wp/v2/posts?search='
-    print(full_url)
-    wordpress_legitimate_list.append(thisfilename)
-    #inp = input('press any key to continue')
-    print('************')
+    break
+  print(base_url)
+  print(base_url2)
+  full_url = base_url + '/wp-json/wp/v2/posts?search='
+  print(full_url)
+  wordpress_legitimate_list.append(thisfilename)
+  #inp = input('press any key to continue')
+  print('************')
 #outfile = open("outfile_wplegit.txt",'w')
 #outfile.write(json.dumps(wordpress_legitimate_list))
 #outfile.close
 #print(wordpress_legitimate_list)
 langcount = 0
-for i in langhash.keys():
-    print(str(i) + ' | ' + str(langhash[i]))
-    langcount += langhash[i]
+for key, value in langhash.items():
+  print(str(key) + ' | ' + str(value))
+  langcount += value
 print(langcount)
-outfile = open("language_hash.json",'w')
-outfile.write(json.dumps(language_hash))
-outfile.close
+with open('language_hash.json','w',encoding='utf-8') as myoutfile:
+  myoutfile.write(json.dumps(language_hash))
